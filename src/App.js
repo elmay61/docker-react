e87f37f97373493ae7d990b17f116c3a25c9c88e
[Elaine May] <[elainelmay@gmail.com]>
2021-04-11 07:35:08 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React easy for real
+          Learn React easy y'all!
         </a>
       </header>
     </div>
